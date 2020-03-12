6f345b9cfdac8bcbd17cc13f9ad7d3ab11eabc92
Noble Obioma <nobioma1@gmail.com>
2020-03-12 23:26:30 +0100

    create user route with endpoint to create new user

---
+++ users-service/src/routes/index.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import { Users } from '#root/db/models';
+
+const usersRouter = Router();
+
+usersRouter.post('/users', async (req, res, next) => {
+  if (!req.body.name && !req.body.email && !req.body.password) {
+    return next(new Error("'name', 'email', 'password' are required"));
+  }
+
+  try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    return res.json({ user: newUser });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+export default usersRouter;
