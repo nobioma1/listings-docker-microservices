334bea146339d6b1c414feedfec0970abeb67b2f
Noble Obioma <nobioma1@gmail.com>
2020-03-18 17:46:51 +0100

    Fix UUID deprecated requiring

--- users-service/src/routes/index.js
+++ users-service/src/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { compareSync } from 'bcryptjs';
-import uuidv4 from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 import { addHours } from 'date-fns';
 
 import { User, UserSessions } from '#root/db/models';
