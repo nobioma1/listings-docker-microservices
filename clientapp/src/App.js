e6114b5c8841fb7440156f5e1e4c58ccde722eb7
Noble Obioma <nobioma1@gmail.com>
2020-03-18 12:04:56 +0100

    Create style layout for page

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+import Main from './components/Main';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 const App = () => {
-  return <div>Client App</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Main />
+    </>
+  );
 };
 
 export default App;
---
+++ clientapp/src/components/Main.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  padding: 1rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 0 auto;
+  width: 70rem;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  margin-right: 1rem;
+`;
+const Sidebar = styled.div`
+  flex: 0 auto;
+  width: 20rem;
+`;
+
+const Main = () => {
+  return (
+    <Wrapper>
+      <Container>
+        <Content>This is Content</Content>
+        <Sidebar>Sidebars</Sidebar>
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Main;
