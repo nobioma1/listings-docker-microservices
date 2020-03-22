30dc9087afbbdd1349b914174b3a141b67501dcf
Noble Obioma <nobioma1@gmail.com>
2020-03-22 21:24:33 +0100

    (listings-service) Create route to add new listing

--- listings-service/src/routes/index.js
+++ listings-service/src/routes/index.js
@@ -1,12 +1,30 @@
 import { Router } from 'express';
 
+import ErrorHandler from '../helpers/ErrorHandler';
 import { Listing } from '#root/db/models';
 
 const listingsRouter = Router();
 
-listingsRouter.get('/listings', async (req, res) => {
+listingsRouter.get('/', async (req, res) => {
   const listings = await Listing.findAll();
   return res.json({ listings });
+});
+
+listingsRouter.post('/', async (req, res, next) => {
+  if (!req.body.title && !req.body.description) {
+    throw new ErrorHandler("'title' and 'description' are required");
+  }
+
+  try {
+    const listing = await Listing.create({
+      title: req.body.title,
+      description: req.body.title,
+    });
+
+    res.status(201).json({ listing });
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default listingsRouter;
